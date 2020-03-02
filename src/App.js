4ced868d6fe7305c129975e95950440c14c4da9a
devleni <50588358+devleni@users.noreply.github.com>
2020-03-02 15:08:17 +0100

    update ui

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Recipe from "./Recipe";
 
 function App() {
   return (
-    <Recipe title="Pasta" ingredients={["flour", "water"]} />
+    <Recipe
+      title="Pasta"
+      ingredients={["flour", "water"]}
+      instructions="Make pasta and cook it."
+      img="spaghetti.jpg"
+    />
   );
 }
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,17 +1,26 @@
 import React, {Component} from "react";
+import "./Recipe.css";
 
 class Recipe extends Component {
   render() {
-    const {title} = this.props;
+    const {title, img, instructions} = this.props;
     const ingredients = this.props.ingredients.map((ingredient, index) => (
       <li key={index}>{ingredient}</li>
     ));
     return (
-      <div>
-        <h1>{title}</h1>
-        <ul>
-          {ingredients}
-        </ul>
+      <div className="recipe-card">
+        <div className="recipe-card-img">
+          <img src={img} alt={title} />
+        </div>
+        <div className="recipe-card-content">
+          <h2 className="recipe-title">{title}</h2>
+          <h3>Ingredients:</h3>
+          <ul>
+            {ingredients}
+          </ul>
+          <h3>Instructions:</h3>
+          <p>{instructions}</p>
+        </div>
       </div>
     );
   }
